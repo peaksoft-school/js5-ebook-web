50b87c6d892f5ad4108edb36b529ed3e69277b18
unknown <burulaiit>
2022-07-08 16:46:14 +0600

    new checkbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import Test from './test'
 
-
 function App() {
-<<<<<<< HEAD
-  return (
-    <div className="App">
-      
-    </div>
-  );
-=======
    return (
       <div className="App">
          <Test />
       </div>
    )
->>>>>>> 0b624e9957aa854abdd0c7be9cdc666d59af969a
 }
 
 export default App
--- src/Components/UI/checkBox/CheckBox.js
+++ src/Components/UI/checkBox/CheckBox.js
@@ -1,43 +1,31 @@
-import React from "react";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import MuiCheckbox from "@mui/material/Checkbox";
-import { styled } from "@mui/material";
-import { useState } from "react";
-const CheckBox = (props) => {
-  const [state,setState]=useState('')
+import FormGroup from '@mui/material/FormGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import MuiCheckbox from '@mui/material/Checkbox'
+import { styled } from '@mui/material'
 
-  const checkBoxHandler=(e)=>{
-    setState(e.target.value)
-  }
-  console.log(state);
-  return (
-    <FormGroup>
-    
-      <FormLabelStyle control={<ChesckboxStyle  value={props.genres} />} label={props.genres} onChange={checkBoxHandler}/>
-   
-    </FormGroup>
-  );
-};
+const CheckBox = () => {
+   return (
+      <FormGroup>
+         <FormLabelStyle control={<ChesckboxStyle />} label="label" />
+      </FormGroup>
+   )
+}
 
-export default CheckBox;
+export default CheckBox
 
 const FormLabelStyle = styled(FormControlLabel)`
-  .MuiFormControlLabel-label {
-    color: black;
-    font-size: 16px;
-   
-  }
-`;
+   .MuiFormControlLabel-label {
+      color: black;
+      font-size: 16px;
+   }
+`
 const ChesckboxStyle = styled(MuiCheckbox)`
-  .MuiSvgIcon-root {
-    width: 26px;
-    height: 26px;
-   
-    
-  }
+   .MuiSvgIcon-root {
+      width: 26px;
+      height: 26px;
+   }
 
-  &.Mui-checked {
-    color: #f34901;
-  }
-`;
+   &.Mui-checked {
+      color: #f34901;
+   }
+`
