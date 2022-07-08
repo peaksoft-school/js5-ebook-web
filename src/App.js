93d05af84cd7f37010b8a7ce6b7931f74de93a0e
jumagylov <jumagylov655@gmail.com>
2022-07-08 12:27:35 +0300

    serach input, inputName, inputEye

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Input from "./Components/input/Input";
+import InputName from "./Components/input/InputName";
+import Search from "./Components/SearchInput/Search";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-     
+      <Search/>
+      <InputName onChange={(e) => props.onChange(e.target.value)} value={props.value}/>
+      <Input onChange={(e) => props.onChange(e.target.value)} value={props.value}/>
     </div>
   );
 }
 
 export default App;
+
+// https://snipp.ru/html-css/show-hide-password 
---
+++ src/Components/input/InputName.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import styled from 'styled-components';
+
+function InputName() {
+  return (
+    <div>
+            <InputName1 type='text' placeholder="Напишите ваше имя"/>
+    </div>
+  )
+}
+
+export default InputName;
+
+const InputName1 = styled.input`
+  width: 514px;
+   height: 38px;
+   padding-left: 18px;
+   border: 1px solid #C4C4C4;
+   margin-top: 19px;
+
+   &:hover {
+    border: 1px solid blue;
+   }
+   &:active, :focus {
+    outline: none;
+    border: 1px solid red;
+   }
+`
+
