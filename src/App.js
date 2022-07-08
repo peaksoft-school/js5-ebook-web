be19319a8708d4c53aa736d71b8d628cc83954a7
atambekovna <atambekovna1411@gmail.com>
2022-07-08 12:39:53 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import BasicModal from "./Components/UI/modal/BasicModal";
+
 
 function App() {
 
   return (
     <div className="App">
-      <BasicModal></BasicModal>
+      
     </div>
   );
 }
