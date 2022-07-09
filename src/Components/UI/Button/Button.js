152ad798ecd5921d0898e2907700cc5cbd6ac553
Kadyrbaev <sadyr_9494@mail.ru>
2022-07-10 04:12:50 +0600

    update button

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,61 +1,32 @@
-import MuiButton from '@mui/material/Button'
-import { styled } from '@mui/material'
+import styled from '@emotion/styled'
 
-const ButtonComponent = ({ children, ...props }) => {
-   return <ButtonStyle {...props}>{children}</ButtonStyle>
+export default function Button({ children, ...props }) {
+   return (
+      <div open={props.open}>
+         <ButtonStyle disableRipple {...props}>
+            {children}
+         </ButtonStyle>
+      </div>
+   )
 }
 
-export default ButtonComponent
-
-const ButtonStyle = styled(MuiButton)`
-   box-shadow: none;
-   border-radius: 0px;
-   font-weight: 600;
-   font-family: open sans-serif;
-   font-style: normal;
-
-   &.MuiButton-backgroundblack {
-      padding: 10px, 24px, 10px, 24px;
-      background: #1c1c1c;
-      padding: 10px 24px;
-      height: 42px;
-      width: 99px;
-      color: #ffffff;
-      font-size: 16px;
-      &:hover {
-         background: #484848;
-      }
-      &:active {
-         background: #f34901;
-      }
-   }
-   &.MuiButton-backgroundred {
-      background: #f34901;
-      width: 242px;
-      height: 33px;
-      padding: 8px, 40px, 8px, 40px;
-      color: #ffffff;
-      font-size: 14px;
-      &:hover {
-         background: #fe6f33;
-      }
-      &:active {
-         background: #e54400;
-      }
-   }
-   &.MuiButton-default {
-      padding: 10px, 16px, 10px, 16px;
-      background: grey;
-      padding: 10px 24px;
-      height: 39px;
-      width: 153px;
-      color: #c4c4c4;
-      font-size: 14px;
-   }
-   &:hover {
-      background: #fe6f33;
-   }
-   &:active {
-      background: #e54400;
-   }
-`
+const ButtonStyle = styled('button')((props) => ({
+   color: props.color || '#ffffff',
+   background: props.background || ' #1c1c1c',
+   width: props.width || '99px',
+   height: props.height || '42px',
+   fontFamily: 'Open Sans',
+   padding: props.padding || '10px 24px',
+   fontSize: props.fontSize || '16px',
+   fontWeight: '600',
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+   border: 'none',
+   '&:hover': {
+      background: props.hover ? '#FE6F33' : '#484848',
+   },
+   '&:active': {
+      background: props.hover ? '#E54400' : ' #F34901',
+   },
+}))
