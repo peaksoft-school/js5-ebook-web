b498a7a11005a319bed360aa1efe4146f7f6ae51
Kadyrbaev <sadyr_9494@mail.ru>
2022-07-11 18:19:30 +0600

    fixed

--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -1,32 +1,46 @@
-import styled from '@emotion/styled'
+import MuiButton from '@mui/material/Button'
+import { styled } from '@mui/material'
 
-export default function Button({ children, ...props }) {
+const ButtonComp = ({ variant, children, onClick, ...props }) => {
    return (
-      <ButtonStyle disableRipple {...props}>
+      <ButtonStyle variant={variant} onClick={onClick} {...props}>
          {children}
       </ButtonStyle>
    )
 }
 
-const ButtonStyle = styled('button')((props) => ({
-   color: props.color || '#ffffff',
-   background: props.background || ' #1c1c1c',
-   width: props.width || '99px',
-   height: props.height || '42px',
-   fontFamily: 'Open Sans',
-   padding: props.padding || '10px 24px',
-   fontSize: props.fontSize || '16px',
-   fontWeight: '600',
-   display: 'flex',
-   justifyContent: 'center',
-   alignItems: 'center',
-   border: 'none',
-   '&:hover': {
-      background: '#484848',
-      ...props.hover,
-   },
-   '&:active': {
-      background: ' #F34901',
-      ...props.active,
-   },
-}))
+export default ButtonComp
+
+const ButtonStyle = styled(MuiButton)`
+   text-transform: capitalize;
+   padding: 8px 40px 8px 40px;
+   border: none;
+   font-weight: 600;
+   font-family: 'Open Sans';
+   line-height: 16.8px;
+   font-style: normal;
+   color: #ffffff;
+   line-height: 120%;
+   gap: 10px;
+   margin-top: 12px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   border-radius: 0px;
+
+   background: ${(props) =>
+      props.variant === 'default' ? '#1c1c1c' : '#f34901'};
+   width: ${(props) => (props.variant === 'default' ? '99px' : '224px')};
+   padding: ${(props) => (props.variant === 'default' ? '10px 24px ' : ' ')};
+   height: ${(props) => (props.variant === 'default' ? '42px' : ' 33px')};
+   font-size: ${(props) => (props.variant === 'default' ? '16px' : '14px')};
+
+   &:hover {
+      background: ${(props) =>
+         props.variant === 'default' ? ' #484848' : '#fe6f33'};
+   }
+   &:active {
+      background: ${(props) =>
+         props.variant === 'default' ? '#f34901' : '#f34901'};
+   }
+`
