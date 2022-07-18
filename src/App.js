672db57b21520dddd6a94d5afa0aaf8b9b44ff23
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2022-07-18 18:40:27 +0600

    clinup App

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { Switch, Route, Redirect } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
-import Admin from './layouts/Admin'
-import Client from './layouts/Client'
-import Vendor from './layouts/Vendor'
-
 function App() {
-   return (
-      <div className="App">
-         <BrowserRouter>
-            <Switch>
-               <Route path="/" exact>
-                  <Redirect to="/Client" />
-               </Route>
-               <Route path="/Admin">
-                  <Admin />
-               </Route>
-               <Route path="/Vendor">
-                  <Vendor />
-               </Route>
-               <Route path="/Client">
-                  <Client />
-               </Route>
-            </Switch>
-         </BrowserRouter>
-      </div>
-   )
+   return <div className="App"></div>
 }
 
 export default App
