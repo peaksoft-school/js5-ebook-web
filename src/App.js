063a4db71d2c0d03912e6313ef73712e797264d9
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2022-07-20 13:26:22 +0600

    clear App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,5 @@
-import { Switch, Route, Redirect } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
-import AdminLayout from './layouts/AdminLayout'
-import ClientLayout from './layouts/ClientLayout'
-import VendorLayout from './layouts/VendorLayout'
-
 function App() {
-   return (
-      <div className="App">
-         <BrowserRouter>
-            <Switch>
-               <Route path="/" exact>
-                  <Redirect to="/Client" />
-               </Route>
-               <Route path="/Admin">
-                  <AdminLayout />
-               </Route>
-               <Route path="/Vendor">
-                  <VendorLayout />
-               </Route>
-               <Route path="/Client">
-                  <ClientLayout />
-               </Route>
-            </Switch>
-         </BrowserRouter>
-      </div>
-   )
+   return <div className="App"></div>
 }
 
 export default App
