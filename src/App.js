8bd9ce336a8fe4b334fc06d0fa1dc6016424fe0d
unknown <burulaiit>
2022-07-21 13:08:01 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import Test from './test'
+import "./App.css"
 
 function App() {
    return (
       <div className="App">
-         <Test />
+        
       </div>
    )
 }
 
 export default App
+
---
+++ src/Components/admin-applications/adminApplications.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const adminApplications = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default adminApplications;
