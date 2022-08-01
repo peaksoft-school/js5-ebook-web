3cb0c063bd8853697daece13a8746478f2f89e5d
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2022-08-01 21:10:50 +0600

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Modal from './Components/UI/Modal'
-import SignUpVendor from './Components/UI/signUp/SignUpVendor'
-
 function App() {
-   return (
-      <div className="App">
-         <Modal open justifyContent="flex-start" height="90vh">
-            <SignUpVendor />
-         </Modal>
-      </div>
-   )
+   return <div className="App" />
 }
 
 export default App
