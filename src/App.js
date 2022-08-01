25426f22819055eb98497e69f07eeb159d7b7739
unknown <burulaiit>
2022-08-01 23:18:52 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 
 import './App.css'
-import AdminLayout from './layouts/AdminLayout'
+
 
 
 
 function App() {
    return (
       <div className="App">
-         <AdminLayout/>
-        
+      
          
          
       </div>
