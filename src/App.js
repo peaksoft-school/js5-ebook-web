d0c46476d90d1a9e69a443d054759062e621b61f
jumagylov <jumagylov655@gmail.com>
2022-08-03 14:15:47 +0300

    feature/userinnerpage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import './App.css'
-import { InnerPage } from './Components/UI/UserInnerPage/InnerPage'
-import { Route,  Redirect } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
-import {BooksPage} from './Components/UI/UserInnerPage/BooksPage'
-import Message from './Components/UI/Message/Message'
+
 function App() {
 
    return (
       <div className="App">
-      {/* <BrowserRouter> */}
-            {/* <VendorLayout>
-               {/* <Route path="/">
-                  <Redirect to="home" />
-               </Route>
-               <Route exact path="/home">
-                  <BooksPage />
-               </Route> */}
-               {/* <Route path="/book-detail/:bookId">
-                  <InnerPage />
-               </Route> */}
-            {/* </VendorLayout> */}
-         {/* </BrowserRouter>  */}
-         <Message />
                </div>
    )
 
