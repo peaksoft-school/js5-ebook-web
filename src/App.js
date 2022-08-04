d4616c2881ef3af5880c26a354ebf3b261e15116
jumagylov <jumagylov655@gmail.com>
2022-08-04 15:32:20 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css'
 
 function App() {
-<<<<<<< HEAD
+
    return (
       <div className="App">
       </div>
    )
-=======
-   return <div className="App"></div>
->>>>>>> bfd68ca9271574d637807d97114f43e11720b336
+
 }
 
 export default App
