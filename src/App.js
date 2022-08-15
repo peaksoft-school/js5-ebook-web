dafadff143779b994c441c113704778734933dbf
tilekkuttubaev <tilek.kuttubaev@gmail.com>
2022-08-15 17:37:50 +0300

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import styled from 'styled-components'
-import './App.css'
-import { BooksCard } from './containers/BookCard'
-import { books } from './containers/books'
-
 function App() {
-   return (
-      <div className="App">
-         <Div>
-            {books.map((elem) => {
-               return <BooksCard book={elem} />
-            })}
-         </Div>
-      </div>
-   )
+   return <div className="App" />
 }
 
 export default App
-
-const Div = styled('div')`
-   border: 1px solid red;
-   max-width: calc(1240px - 282px);
-   margin: 0 auto;
-   display: flex;
-   flex-wrap: wrap;
-`
