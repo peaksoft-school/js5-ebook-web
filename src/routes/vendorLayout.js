63064d8c5930a120bd14ac9005c09a359c984938
atambekovna <atambekovna1411@gmail.com>
2022-08-18 18:55:17 +0600

    test

--- src/routes/vendorLayout.js
+++ src/routes/vendorLayout.js
@@ -1,15 +1,13 @@
 import { Route } from 'react-router'
-import MainPage from '../containers/mainPage/MainPage'
 import VendorLayout from '../layouts/VendorLayout'
-// import { BooksPage } from '../Components/vendor/BooksPage'
-// import { InnerPage } from '../Components/vendor/InnerPage'
+import { BooksPage } from '../Components/vendor/BooksPage'
+import { InnerPage } from '../Components/vendor/InnerPage'
 
 export default function vendorLayout() {
    return (
       <Route path="/" element={<VendorLayout />}>
-         <Route path="main" element={<MainPage />} />
-         {/* <Route path="books" element={<BooksPage />} />
-         <Route path="books/:bookId" element={<InnerPage />} /> */}
+         <Route path="books" element={<BooksPage />} />
+         <Route path="books/:bookId" element={<InnerPage />} />
       </Route>
    )
 }
