6c26c1253dec65161dc4b175a507fe7822cdd2de
atambekovna <atambekovna1411@gmail.com>
2022-08-22 17:28:37 +0600

    add book image slider component

--- src/Components/UI/MainPageslider.js
+++ src/Components/UI/MainPageslider.js
@@ -1,30 +1,40 @@
-import { useState } from 'react'
-import Slider from 'react-slick'
-import { styled } from '@mui/material'
-import Vector from '../../assets/icons/next.svg'
-import Vector2 from '../../assets/icons/prev.svg'
+//
 
-const NextArrow = () => {
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useState } from 'react'
+import { styled } from '@mui/material'
+import AboutSlider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { ReactComponent as NextIcon } from '../../assets/icons/slider/next.svg'
+import { ReactComponent as ArrowIcon } from '../../assets/icons/slider/prev.svg'
+
+const NextArrow = ({ onClick }) => {
    return (
-      <div className="arrow next">
-         <img src={Vector} alt="book Vector" />
+      // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+      <div className="arrow next" onClick={onClick}>
+         <NextIcon />
       </div>
    )
 }
-const PrevArrow = () => {
+// eslint-disable-next-line react/no-unstable-nested-components
+const PrevArrow = ({ onClick }) => {
    return (
-      <div className="arrow prev">
-         <img src={Vector2} alt="book Vector2" />
+      // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+      <div className="arrow prev" onClick={onClick}>
+         <ArrowIcon />
       </div>
    )
 }
 function MainPageSlider({ images }) {
    const [imageIndex, setImageIndex] = useState(0)
-
    const settings = {
+      dots: false,
       infinite: true,
-      lazyLoad: true,
-      speed: 300,
+      lazyLoad: false,
+      speed: 900,
+      autoplay: false,
+      autoplaySpeed: 3000,
       slidesToShow: 3,
       tabindex: -1,
       centerMode: true,
@@ -33,71 +43,128 @@ function MainPageSlider({ images }) {
       prevArrow: <PrevArrow />,
       beforeChange: (current, next) => setImageIndex(next),
    }
-
    return (
       <Container className="slider">
-         <Slider {...settings}>
+         <AboutSlider {...settings}>
             {images.map((img, idx) => (
-               <div className={idx === imageIndex ? 'activeSlide' : 'slide'}>
-                  <Image src={img} alt={img} />
+               <div
+                  key={img.id}
+                  className={idx === imageIndex ? 'activeSlide' : 'slide'}
+               >
+                  <Image src={img.image} alt={img} />
+                  <BookInfoBlock>
+                     <BookInfoItem>
+                        <Title>{img.name}</Title>
+                     </BookInfoItem>
+                     <BookInfoItem width="80%">
+                        <Author>{img.author}</Author>
+                     </BookInfoItem>
+                     <BookInfoItem width="20%">
+                        <Price>{img.price}</Price>
+                     </BookInfoItem>
+                  </BookInfoBlock>
                </div>
             ))}
-         </Slider>
+         </AboutSlider>
       </Container>
    )
 }
-
 export default MainPageSlider
-
+const Title = styled('h3')`
+   font-family: 'Open Sans';
+   font-weight: 400;
+   font-size: 16px;
+   color: #d6d6d6;
+   padding: 0;
+   padding-top: 20px;
+   margin: 0;
+`
+const Author = styled(Title)`
+   font-size: 14px;
+   color: #8c8c8c;
+   padding-top: 10px;
+`
+const Price = styled(Title)`
+   font-weight: 600;
+   color: #ff4c00;
+   text-align: right;
+   padding-top: 10px;
+`
+const BookInfoItem = styled('div')`
+   /* border: 1px solid red; */
+   /* height: 30px; */
+   margin: 0;
+   padding: 0;
+   width: ${(props) => props.width || '100%'};
+`
+const BookInfoBlock = styled('div')`
+   /* border: 1px solid red; */
+   width: 292px;
+   display: flex;
+   justify-content: space-between;
+   flex-wrap: wrap;
+   opacity: 0;
+   transition: ease-in 0.2s;
+`
 const Container = styled('div')`
-   width: 632px;
-   margin: 10rem auto;
-
+   width: 480px;
+   /* margin: 97px auto; */
+   /* margin-bottom: 155px; */
+   /* border: 1px solid red; */
    .slide {
-      transform: scale(0.6);
-      transition: transform 600ms;
-      opacity: 0.5;
-      z-index: -10;
-      margin-left: -10px;
+      transform: scale(0.5);
+      /* transition: ease-in transform 900ms; */
+      transition: 900ms;
+      margin-left: -35px;
+      position: relative;
+      z-index: 0;
    }
-
+   .arrow.next {
+      /* border: 1px solid aqua; */
+      background-color: rgba(0, 0, 0, 0);
+      right: -150px;
+   }
+   .arrow.prev {
+      /* border: 1px solid aqua; */
+      background-color: rgba(0, 0, 0, 0);
+      left: -150px;
+   }
    .activeSlide {
-      transform: scale(1);
-      z-index: 1;
-      opacity: 1;
-      margin-left: -16px;
+      /* transform: scale(1); */
+      /* transition: transform 900ms; */
+      transition: 900ms;
+      margin-left: -65px;
+      position: relative;
+      z-index: 10;
+      & > div {
+         opacity: 1;
+      }
    }
-
    .arrow {
-      background-color: #fff;
+      /* background-color: #fff; */
       position: absolute;
       cursor: pointer;
       z-index: 10;
    }
-
    .arrow svg {
       transition: color 300ms;
    }
-
    .arrow svg:hover {
       color: #68edff;
    }
-
    .next {
       right: -20%;
-      top: 50%;
+      top: 45%;
       color: red;
    }
-
    .prev {
       left: -16%;
-      top: 50%;
+      top: 45%;
       color: red;
    }
 `
-
 const Image = styled('img')`
    width: 292px;
-   height: 468pxpx;
    margin: 0 auto;
+   transition: ease-in 0.2s;
 `
--- src/containers/clientMainPage/MainPage.js
+++ src/containers/clientMainPage/MainPage.js
@@ -1,10 +1,15 @@
 import styled from '@emotion/styled'
+import Button from '../../Components/UI/Button/Button'
+import InputText from '../../Components/UI/Inputs/InputText'
 import MainPageSlider from '../../Components/UI/MainPageslider'
+import { books } from './bookImage'
 
 function App() {
    return (
-      <div className="App">
-         <StyledSliderCont>slider</StyledSliderCont>
+      <StyledMainPage>
+         <StyledSliderCont>
+            <MainPageSlider images={books} />
+         </StyledSliderCont>
 
          <StyledBestSlider>
             <StyledTitle>
@@ -27,7 +32,7 @@ function App() {
                   </div>
                </StyledBookTitle>
                <StyledBookSlider>
-                  <MainPageSlider />
+                  <MainPageSlider images={books} />
                </StyledBookSlider>
             </StyledBookSliderBlock>
          </StyledBestSlider>
@@ -67,21 +72,114 @@ function App() {
                </StyledBookDescriptionBlock>
             </StyledBookSliderBlock>
          </StyledLatestPublCont>
-      </div>
+         <StyledAudioBooksCont>
+            <StyledTitle>
+               <StyledBestText>Аудиокниги</StyledBestText>
+               <StyledNavLink href="/">Смотреть все</StyledNavLink>
+            </StyledTitle>
+            <StyledBookCardCont>
+               <StyledBookCard className="first">
+                  <img
+                     src="https://cv9.litres.ru/pub/c/audiokniga/cover_415/37402792-dzhen-sinsero-ni-sy-vostochnaya-mudrost-kotoraya-glasit-bud-uvere-37402792.jpg"
+                     alt="book"
+                  />
+                  <StyledBookCardName>НИ СЫ</StyledBookCardName>
+                  <StyledBookCardAuthor>Джен Синсеро</StyledBookCardAuthor>
+                  <StyledBookCardTitle>
+                     <StyledBookCardTime>
+                        19 ч. 44 мин. 19 сек.
+                     </StyledBookCardTime>
+                     <StyledBookPrice>234 с</StyledBookPrice>
+                  </StyledBookCardTitle>
+               </StyledBookCard>
+               <StyledBookCard className="second">
+                  <img
+                     src="https://img-gorod.ru/28/363/2836344_detail.jpg"
+                     alt="book"
+                  />
+                  <StyledBookCardName>НИ СЫ</StyledBookCardName>
+                  <StyledBookCardAuthor>Джен Синсеро</StyledBookCardAuthor>
+                  <StyledBookCardTitle>
+                     <StyledBookCardTime>
+                        19 ч. 44 мин. 19 сек.
+                     </StyledBookCardTime>
+                     <StyledBookPrice>234 с</StyledBookPrice>
+                  </StyledBookCardTitle>
+               </StyledBookCard>
+               <StyledBookCard>
+                  <img
+                     src="https://cm.author.today/content/2021/11/23/48cb14773d40404c8200e22fb14fc4af.jpg"
+                     alt="book"
+                  />
+                  <StyledBookCardName>НИ СЫ</StyledBookCardName>
+                  <StyledBookCardAuthor>Джен Синсеро</StyledBookCardAuthor>
+                  <StyledBookCardTitle>
+                     <StyledBookCardTime>
+                        19 ч. 44 мин. 19 сек.
+                     </StyledBookCardTime>
+                     <StyledBookPrice>234 с</StyledBookPrice>
+                  </StyledBookCardTitle>
+               </StyledBookCard>
+            </StyledBookCardCont>
+         </StyledAudioBooksCont>
+         <StyledBestSlider>
+            <StyledTitle>
+               <StyledBestText>Электронные книги</StyledBestText>
+               <StyledNavLink href="/">Смотреть все</StyledNavLink>
+            </StyledTitle>
+            <StyledBookSliderBlock>
+               <StyledBookTitle>
+                  <StyledBookName>Тонкое искусство пофигизма</StyledBookName>
+                  <StyledBookDescription>
+                     Современное общество пропагандирует культ успеха: будь
+                     умнее, богаче, продуктивнее— будь лучше всех. Соцсети
+                     изобилуют историями натему, как какой-томалец придумал.
+                     Соцсети изобилуют историями натему, как какой-томалец
+                     придумал приложение…
+                  </StyledBookDescription>
+                  <div>
+                     <StyledNavLink href="/">Подробнее</StyledNavLink>
+                     <StyledBookPrice>234 с</StyledBookPrice>
+                  </div>
+               </StyledBookTitle>
+               <StyledBookSlider>
+                  <MainPageSlider images={books} />
+               </StyledBookSlider>
+            </StyledBookSliderBlock>
+         </StyledBestSlider>
+         <StyledMainFooter>
+            <StyledMailingCont>
+               <p>Подписаться на рассылку</p>
+               <div>
+                  <InputText placeholder="Напишите ваш E-mail" />
+                  <Button variant="universal" background="black" width="137px">
+                     Отправить
+                  </Button>
+               </div>
+            </StyledMailingCont>
+            <StyledSocialMedia>
+               <p>Instagram</p>
+               <p>Facebook</p>
+               <p>Bконтакте</p>
+            </StyledSocialMedia>
+         </StyledMainFooter>
+      </StyledMainPage>
    )
 }
 
 export default App
 
+const StyledMainPage = styled.div`
+   width: 100%;
+   margin-top: 27px;
+`
 const StyledSliderCont = styled.div`
-   /* width: 1440.5px;
-  height: 720px; */
    background: #1c1c1c;
    padding: 97px 304px 155px 304px;
+   display: flex;
+   justify-content: center;
 `
 const StyledBestSlider = styled.div`
-   /* width: 1440px; */
-   /* height: 960px; */
    padding: 150px 80px;
 `
 const StyledTitle = styled.div`
@@ -185,7 +283,10 @@ const StyledGenresText = styled.p`
 `
 const StyledBookImage = styled.div`
    width: 342px;
-   transform: rotate(9.34deg);
+   transition: transform 1s ease 0s;
+   &:hover {
+      transform: rotate(9.34deg);
+   }
 `
 const StyledBookDescriptionBlock = styled.div`
    width: 365px;
@@ -211,4 +312,92 @@ const StyledLatestBookDesc = styled.p`
    font-size: 16px;
    line-height: 130%;
    color: #d1d1d1;
+`
+const StyledAudioBooksCont = styled.div`
+   width: 100%;
+   padding: 150px 80px 0px 80px;
+`
+const StyledBookCardTitle = styled.div`
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+`
+const StyledBookCardCont = styled.div`
+   width: 1280px;
+   height: 747px;
+   display: flex;
+   justify-content: space-between;
+   .first {
+      align-self: flex-end;
+   }
+   .second {
+      align-self: center;
+   }
+`
+const StyledBookCard = styled.div`
+   width: 309px;
+   display: flex;
+   flex-direction: column;
+`
+const StyledBookCardName = styled.span`
+   font-family: 'Open Sans';
+   font-style: normal;
+   font-weight: 600;
+   font-size: 14px;
+   line-height: 130%;
+   text-transform: uppercase;
+   color: #222222;
+`
+const StyledBookCardAuthor = styled.span`
+   font-family: 'Open Sans';
+   font-style: normal;
+   font-weight: 400;
+   font-size: 14px;
+   line-height: 130%;
+   color: #334433;
+`
+const StyledBookCardTime = styled.span`
+   font-family: 'Open Sans';
+   font-style: normal;
+   font-weight: 400;
+   font-size: 14px;
+   line-height: 130%;
+   color: #8c8c8c;
+`
+const StyledMainFooter = styled.div`
+   /* height: 440px; */
+   padding: 150px 0px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   align-items: center;
+`
+const StyledMailingCont = styled.div`
+   width: 1091px;
+   height: 111px;
+   display: flex;
+   flex-direction: column;
+   justify-content: space-between;
+   & p {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 600;
+      font-size: 20px;
+      line-height: 120%;
+      color: #222222;
+   }
+   & div {
+      display: flex;
+   }
+`
+const StyledSocialMedia = styled.div`
+   display: flex;
+   justify-content: space-between;
+   width: 701px;
+   font-family: 'Open Sans';
+   font-style: normal;
+   font-weight: 400;
+   font-size: 22px;
+   line-height: 130%;
+   color: #222222;
 `
