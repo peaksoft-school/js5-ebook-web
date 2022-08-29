be1bc3a7c54d5401bf71e3d924c2878699b5b8a9
jumagylov <jumagylov655@gmail.com>
2022-08-29 17:05:14 +0300

    update routes

--- src/routes/clientLayout.js
+++ src/routes/clientLayout.js
@@ -1,12 +1,10 @@
 import { Route } from 'react-router'
 import ClientLayout from '../layouts/ClientLayout'
-import { BooksPage } from '../containers/UserInnerPage/BooksPage'
 import { UserInnerPage } from '../containers/UserInnerPage/UserInnerPage'
 
 export default function clientLayout() {
    return (
       <Route path="/" element={<ClientLayout />}>
-         <Route path="books" element={<BooksPage />} />
          <Route path="/books/:bookId" element={<UserInnerPage />} />
       </Route>
    )
