7e1d7d848f2418014750d8e6e5da7da0fd0eefba
unknown <burulaiit>
2022-08-30 16:02:05 +0600

    a

--- src/routes/adminLayout.js
+++ src/routes/adminLayout.js
@@ -1,12 +1,11 @@
 import { Route } from 'react-router'
+import { InnerPageAdminApplication } from '../containers/adminContainers/adminInnerPage/InnerPageAdminApplication'
 import AdminLayout from '../layouts/AdminLayout'
 
 export default function adminLayout() {
    return (
       <Route path="/" element={<AdminLayout />}>
-         {/* <Route path="request" element={<AdminApplications />} />
-         <Route path="request/:id" element={<InnerPageAdminApplication />} />
-         <Route path="/books" element={<AdminBooks />} /> */}
+         <Route path="innerpage" element={<InnerPageAdminApplication />} />
       </Route>
    )
 }
