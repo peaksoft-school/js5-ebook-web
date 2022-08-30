6051b8ce3b118de2af97c88ca838ec8f5557e63d
unknown <burulaiit>
2022-08-30 15:05:49 +0600

    update

--- src/Components/UI/Modal.jsx
+++ src/Components/UI/Modal.jsx
@@ -10,6 +10,7 @@ export default function Modal({
    width,
    height,
    justifyContent,
+   ...props
 }) {
    return (
       <MuiModal
@@ -23,6 +24,7 @@ export default function Modal({
             width={width}
             height={height}
             justifyContent={justifyContent}
+            {...props}
          >
             {children}
          </StyledBox>
@@ -44,7 +46,7 @@ const StyledBox = styled(Box)`
    justify-content: ${(props) => props.justifyContent || 'center'};
    flex-direction: column;
    align-items: center;
-   overflow: auto;
+   overflow: ${(props) => props.overflow || 'auto'};
    ::-webkit-scrollbar {
       width: 5px;
    }
