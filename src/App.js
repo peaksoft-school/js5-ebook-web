3b628f108cf7d2d0c86f83fe7bd02cb0c417851c
unknown <burulaiit>
2022-08-30 15:22:13 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import AppRoutes from './routes/appRoutes'
-import Spinner from './Components/UI/Spinner'
+// import { useEffect, useState } from 'react'
+// import { useSelector } from 'react-redux'
+// import AppRoutes from './routes/AppRoutes'
+// import Spinner from './Components/UI/Spinner'
+
+import AdminBooks from './containers/adminBooks/AdminBooks'
 
 function App() {
-   const status = useSelector((store) => store.auth.status)
-   const [showSpinner, setShowSpinner] = useState()
-   useEffect(() => {
-      if (status === 'pending') {
-         setShowSpinner(true)
-      } else {
-         setShowSpinner(false)
-      }
-   }, [status])
+   // const status = useSelector((store) => store.auth.status)
+   // const [showSpinner, setShowSpinner] = useState()
+   // useEffect(() => {
+   //    if (status === 'pending') {
+   //       setShowSpinner(true)
+   //    } else {
+   //       setShowSpinner(false)
+   //    }
+   // }, [status])
    return (
       <div className="App">
-         {showSpinner && <Spinner />}
-         <AppRoutes />
+         {/* {showSpinner && <Spinner />}
+         <AppRoutes /> */}
+         <AdminBooks />
       </div>
    )
 }
