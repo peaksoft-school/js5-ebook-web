3abcc2462be5e8e0d93910ab5ac30c4e4da56ab4
unknown <burulaiit>
2022-08-30 14:58:43 +0600

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { addBookSlice } from './slices/addBookSlice'
+import { applicationsInnerPageSlices } from './slices/applicationInnerPagesSlices'
 import authSlices from './slices/authSlices'
 
 const store = configureStore({
    reducer: {
       auth: authSlices.reducer,
       addbook: addBookSlice.reducer,
+      applicationsInnerPage: applicationsInnerPageSlices.reducer,
    },
 })
 
