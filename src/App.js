e666a3030dff12d8a09b876126b56289d951bcfe
unknown <burulaiit>
2022-08-30 13:46:48 +0600

    s

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import AppRoutes from './routes/AppRoutes'
-import Spinner from './Components/UI/Spinner'
+// import { useEffect, useState } from 'react'
+// import { useSelector } from 'react-redux'
+// import AppRoutes from './routes/AppRoutes'
+// import Spinner from './Components/UI/Spinner'
+import InnerPageAdminApplication from './containers/adminInnerPage/InnerPageAdminApplication'
 
 function App() {
-   const status = useSelector((store) => store.auth.status)
-   const [showSpinner, setShowSpinner] = useState()
-   useEffect(() => {
-      if (status === 'pending') {
-         setShowSpinner(true)
-      } else {
-         setShowSpinner(false)
-      }
-   }, [status])
+   // const status = useSelector((store) => store.auth.status)
+   // const [showSpinner, setShowSpinner] = useState()
+   // useEffect(() => {
+   //    if (status === 'pending') {
+   //       setShowSpinner(true)
+   //    } else {
+   //       setShowSpinner(false)
+   //    }
+   // }, [status])
    return (
       <div className="App">
-         {showSpinner && <Spinner />}
-         <AppRoutes />
+         {/* {showSpinner && <Spinner />}
+         <AppRoutes /> */}
+         <InnerPageAdminApplication />
       </div>
    )
 }
--- src/routes/adminLayout.js
+++ src/routes/adminLayout.js
@@ -2,5 +2,11 @@ import { Route } from 'react-router'
 import AdminLayout from '../layouts/AdminLayout'
 
 export default function adminLayout() {
-   return <Route path="/" element={<AdminLayout />} />
+   return (
+      <Route path="/" element={<AdminLayout />}>
+         {/* <Route path="request" element={<AdminApplications />} />
+         <Route path="request/:id" element={<InnerPageAdminApplication />} />
+         <Route path="/books" element={<AdminBooks />} /> */}
+      </Route>
+   )
 }
