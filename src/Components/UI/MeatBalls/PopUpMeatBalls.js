93e225c660d58970e390e768322d3de5deedd40e
Kadyrbaev <sadyr_9494@mail.ru>
2022-09-11 06:29:09 +0600

    updating branch

---
+++ src/Components/UI/MeatBalls/PopUpMeatBalls.js
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import { MenuItem } from '@mui/material'
+import styled from 'styled-components'
+import MeadIcon from '../../../assets/icons/MeatBalls/MeatBall.svg'
+import PopUp from '../popup'
+
+const PopUpMeatBalls = ({ options, prop }) => {
+   const [position, setPosition] = useState(null)
+   const [openPopup, setOpenPopup] = useState(false)
+   const toggleHandler = (e) => {
+      if (!prop) {
+         e.stopPropagation()
+      }
+      setPosition(e.currentTarget)
+      setOpenPopup((prev) => !prev)
+   }
+   const closeHandler = () => {
+      setOpenPopup((prev) => !prev)
+   }
+   return (
+      <>
+         <ImageBlock>
+            <Img onClick={toggleHandler} src={MeadIcon} />
+         </ImageBlock>
+         <PopUp open={openPopup} onClose={closeHandler} anchorEl={position}>
+            <WrapperMeatballs>
+               {options.map((i, index) => {
+                  return (
+                     <MenuItem
+                        // eslint-disable-next-line react/no-array-index-key
+                        key={index}
+                        onClick={(e) => {
+                           e.stopPropagation()
+                           closeHandler()
+                           i.onClick(i.id)
+                        }}
+                     >
+                        <Icon>{i.icon}</Icon>
+                        {i.text}
+                     </MenuItem>
+                  )
+               })}
+            </WrapperMeatballs>
+         </PopUp>
+      </>
+   )
+}
+export default PopUpMeatBalls
+
+const Icon = styled('span')`
+   /* border: 1px solid red; */
+   display: inline-block;
+   margin-right: 10px;
+`
+
+const ImageBlock = styled('div')`
+   /* border: 1px solid red; */
+   width: 20px;
+   height: 20px;
+`
+const WrapperMeatballs = styled.div`
+   padding: 3px;
+`
+const Img = styled.img`
+   /* border: 1px solid red; */
+   width: 100%;
+   height: 100%;
+   cursor: pointer;
+   /* height: 5px; */
+`
--- src/store/createActions/vendorMainPagesActions.js
+++ src/store/createActions/vendorMainPagesActions.js
@@ -3,11 +3,11 @@ import appFetch from '../../hooks/AppFetch'
 import snackbarAction from '../slices/snackbarSlice'
 import vendorMainPageAction from '../slices/vendorMainPageSlice'
 
-export const getMainBooks = (slectedText, next) => {
+export const getMainBooks = (slectedText, next, vendorId) => {
    return async (dispatch) => {
       try {
          const getData = await appFetch({
-            url: `/api/vendors/18/books?aboutBooks=${
+            url: `/api/vendors/${vendorId}/books?aboutBooks=${
                slectedText || 'ALL'
             }&page=1&size=${next}`,
          })
@@ -53,20 +53,28 @@ export const getMainBooksDelete = (id) => {
 
 // EDITE
 
-export const putVendorBook = (inputValues, images, bookType, bookId) => {
+export const putVendorBook = (
+   inputValues,
+   images,
+   bookType,
+   bookId,
+   pdfValue
+) => {
    const mydata = {
       mainImage: images.mainImage,
       secondImage: images.secondImage,
       thirdImage: images.thirdImage,
       name: inputValues.name,
-      genreId: inputValues.genreId,
+      genreId: 7,
       price: inputValues.price,
       author: inputValues.author,
       description: inputValues.description,
-      language: inputValues.language,
+      language: 'RUSSIAN',
       yearOfIssue: inputValues.yearOfIssue,
       discount: inputValues.discount,
       fragment: inputValues.fragment,
+      bestseller: true,
+      electronicBook: 'string',
       pageSize: inputValues.pageSize,
       publishingHouse: inputValues.publishingHouse,
       quantityOfBook: inputValues.quantityOfBook,
@@ -83,23 +91,150 @@ export const putVendorBook = (inputValues, images, bookType, bookId) => {
          if (typeof images.mainImage === 'object') {
             const imgFiles = await appFileFetchService(images.mainImage)
             mydata.mainImage = imgFiles.link
+         } else {
+            mydata.mainImage = images.mainImage
          }
          if (typeof images.secondImage === 'object') {
-            const imgFiles = await appFileFetchService(images.mainImage)
-            mydata.mainImage = imgFiles.link
+            const imgFiles = await appFileFetchService(images.secondImage)
+            mydata.secondImage = imgFiles.link
          }
          if (typeof images.thirdImage === 'object') {
-            const imgFiles = await appFileFetchService(images.mainImage)
-            mydata.mainImage = imgFiles.link
+            const imgFiles = await appFileFetchService(images.thirdImage)
+            mydata.thirdImage = imgFiles.link
+         }
+         if (typeof pdfValue === 'object') {
+            const pdf = await appFileFetchService(pdfValue)
+            mydata.electronicBook = pdf.link
          }
          const putData = await appFetch({
             method: 'PUT',
             url: urlBookType,
             body: mydata,
          })
-         dispatch(snackbarAction.snackbarSuccess(putData))
+         if (putData.ok) {
+            dispatch(snackbarAction.snackbarSuccess())
+         }
       } catch (error) {
-         dispatch(snackbarAction.snackbarFalse(error))
+         if (error === 'Что то пошло не так!') {
+            dispatch(snackbarAction.snackbarFalse(error))
+         } else {
+            dispatch(snackbarAction.snackbarSuccess())
+         }
+      }
+   }
+}
+
+export const editeElectronicBook = (inputValues, images, bookId, pdfValue) => {
+   const mydata = {
+      mainImage: images.mainImage,
+      secondImage: images.secondImage,
+      thirdImage: images.thirdImage,
+      name: inputValues.name,
+      genreId: 9,
+      price: inputValues.price,
+      author: inputValues.author,
+      description: inputValues.description,
+      language: 'RUSSIAN',
+      yearOfIssue: inputValues.yearOfIssue,
+      discount: inputValues.discount,
+      fragment: inputValues.fragment,
+      bestseller: true,
+      electronicBook: 'string',
+      pageSize: inputValues.pageSize,
+      publishingHouse: inputValues.publishingHouse,
+      quantityOfBook: inputValues.quantityOfBook,
+   }
+   return async (dispatch) => {
+      try {
+         if (typeof images.mainImage === 'object') {
+            const imgFiles = await appFileFetchService(images.mainImage)
+            mydata.mainImage = imgFiles.link
+         } else {
+            mydata.mainImage = images.mainImage
+         }
+         if (typeof images.secondImage === 'object') {
+            const imgFiles = await appFileFetchService(images.secondImage)
+            mydata.secondImage = imgFiles.link
+         }
+         if (typeof images.thirdImage === 'object') {
+            const imgFiles = await appFileFetchService(images.thirdImage)
+            mydata.thirdImage = imgFiles.link
+         }
+         if (typeof pdfValue === 'object') {
+            const pdf = await appFileFetchService(pdfValue)
+            mydata.electronicBook = pdf.link
+         }
+         const putData = await appFetch({
+            method: 'PUT',
+            url: `/api/book/update/electronicBook/${bookId}`,
+            body: mydata,
+         })
+         if (putData.ok) {
+            dispatch(snackbarAction.snackbarSuccess())
+         }
+      } catch (error) {
+         if (error === 'Что то пошло не так!') {
+            dispatch(snackbarAction.snackbarFalse(error))
+         } else {
+            dispatch(snackbarAction.snackbarSuccess())
+         }
+      }
+   }
+}
+
+export const editeAudioBook = (inputValues, images, bookId) => {
+   const mydata = {
+      mainImage: images.mainImage,
+      secondImage: images.secondImage,
+      thirdImage: images.thirdImage,
+      name: inputValues.name,
+      genreId: 13,
+      price: inputValues.price,
+      author: inputValues.author,
+      description: inputValues.description,
+      language: 'KYRGYZ',
+      yearOfIssue: inputValues.yearOfIssue,
+      discount: inputValues.discount,
+      bestseller: true,
+      fragment: 'string',
+      duration: '02:30:45',
+      audioBook: 'string',
+   }
+   return async (dispatch) => {
+      try {
+         if (typeof images.mainImage === 'object') {
+            const imgFiles = await appFileFetchService(images.mainImage)
+            mydata.mainImage = imgFiles.link
+         } else {
+            mydata.mainImage = images.mainImage
+         }
+         if (typeof images.secondImage === 'object') {
+            const imgFiles = await appFileFetchService(images.secondImage)
+            mydata.secondImage = imgFiles.link
+         }
+         if (typeof images.thirdImage === 'object') {
+            const imgFiles = await appFileFetchService(images.thirdImage)
+            mydata.thirdImage = imgFiles.link
+         }
+         // if (typeof audioValues === 'object') {
+         //    const pdf = await appFileFetchService(audioValues)
+         //    mydata.audioBook = pdf.link
+         //    console.log(pdf)
+         // }
+         const putData = await appFetch({
+            method: 'PUT',
+            url: `/api/book/update/audioBook/${bookId}`,
+            body: mydata,
+         })
+         if (putData.ok) {
+            dispatch(snackbarAction.snackbarSuccess())
+         }
+      } catch (error) {
+         if (error === 'Что то пошло не так!') {
+            dispatch(snackbarAction.snackbarFalse(error))
+         } else {
+            dispatch(snackbarAction.snackbarSuccess())
+         }
       }
    }
 }
