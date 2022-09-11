3d24a38d86150e777d11279ee1226a962d6b2ac8
atambekovna <atambekovna1411@gmail.com>
2022-09-11 22:34:46 +0600

    divided vendors and vendor books files into components

--- src/containers/adminContainers/adminVendors/AdminVendorBooks.js
+++ src/containers/adminContainers/adminVendors/AdminVendorBooks.js
@@ -4,12 +4,11 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import Divider from '@mui/material/Divider'
 import { getAdminVendorBooks } from '../../../store/slices/getAdminVendorsSlice'
-import bookLikeIcon from '../../../assets/icons/bookLike.svg'
-import PopUpMeatBalls from '../../../Components/UI/MeatBalls/PopUpMeatBalls'
 import SelectBooks from '../Admin/SelectBooks'
 import { FilterBooks } from '../../../utils/constants/constants'
 import Button from '../../../Components/UI/Button/Button'
 import Spinner from '../../../Components/UI/Spinner'
+import VendorBookCard from './VendorBookCard'
 
 const selectTitle = [
    {
@@ -98,12 +97,6 @@ const AdminVendorBooks = () => {
       })
    }
 
-   const menuMeatBall = [
-      {
-         text: 'Удалить',
-      },
-   ]
-
    return (
       <div>
          {isShowSpinner && <Spinner />}
@@ -118,29 +111,7 @@ const AdminVendorBooks = () => {
             </div>
          </StyledSelectBlock>
          <Divider />
-         <StyledBookSBlock>
-            {books &&
-               books.map((item) => {
-                  return (
-                     <StyledWrap key={item.id}>
-                        <StyledHeader>
-                           <img src={bookLikeIcon} alt="icon" />
-                           <StyledText>({item.favorite})</StyledText>
-                           <StyledText>В корзине ({item.basket})</StyledText>
-                           <PopUpMeatBalls options={menuMeatBall} />
-                        </StyledHeader>
-                        <StyledImageBlock>
-                           <StyledImage src={item.mainImage} alt="book" />
-                        </StyledImageBlock>
-                        <StyledBookName>{item.name}</StyledBookName>
-                        <StyledBookInfo>
-                           <StyledText>{item.dateOfRegistration}</StyledText>
-                           <StyledPrice>{item.price} c</StyledPrice>
-                        </StyledBookInfo>
-                     </StyledWrap>
-                  )
-               })}
-         </StyledBookSBlock>
+         <VendorBookCard books={books} />
          {showSeeMore && (
             <StyledBtnBlock>
                <Button
@@ -167,12 +138,6 @@ const StyledSelectBlock = styled.div`
    align-items: center;
 `
 
-const StyledBookSBlock = styled.div`
-   width: 100%;
-   display: flex;
-   flex-wrap: wrap;
-   padding: 30px 0;
-`
 const StyledAmount = styled.p`
    font-family: 'Open Sans';
    font-style: normal;
@@ -180,59 +145,6 @@ const StyledAmount = styled.p`
    font-size: 16px;
    line-height: 130%;
    color: #969696;
-`
-const StyledWrap = styled.div`
-   width: 208px;
-   background: #efefef;
-   padding: 14px 34px;
-   margin-bottom: 30px;
-   margin-right: 17px;
-`
-const StyledHeader = styled.div`
-   width: 100%;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-`
-const StyledText = styled.span`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 400;
-   font-size: 14px;
-   line-height: 120%;
-   color: #8a8a8a;
-`
-const StyledPrice = styled.span`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 600;
-   font-size: 16px;
-   line-height: 120%;
-   color: #ff4c00;
-`
-const StyledImageBlock = styled.div`
-   /* border: 1px solid red; */
-   height: 200px;
-   margin: 14px 0 13px 0;
-`
-const StyledImage = styled.img`
-   width: 100%;
-   height: 100%;
-   object-fit: cover;
-`
-const StyledBookInfo = styled.div`
-   width: 100%;
-   display: flex;
-   justify-content: space-between;
-`
-const StyledBookName = styled.span`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 600;
-   font-size: 14px;
-   line-height: 120%;
-   text-transform: uppercase;
-   color: #222222;
 `
 // const StyledMealls = styled(Meatballs)`
 //    align-self: flex-end;
--- src/containers/adminContainers/adminVendors/AdminVendors.js
+++ src/containers/adminContainers/adminVendors/AdminVendors.js
@@ -1,14 +1,3 @@
-import {
-   Table,
-   TableBody,
-   TableCell,
-   TableContainer,
-   TableHead,
-   TableRow,
-   Paper,
-   IconButton,
-} from '@mui/material'
-import styled from '@emotion/styled'
 import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router'
@@ -17,10 +6,8 @@ import {
    deleteAdminVendor,
    cleanVendorDel,
 } from '../../../store/slices/getAdminVendorsSlice'
-import { ReactComponent as DeleteIcon } from '../../../assets/icons/delete.svg'
-import Button from '../../../Components/UI/Button/Button'
-import Modal from '../../../Components/UI/Modal'
 import Spinner from '../../../Components/UI/Spinner'
+import VendorTable from './VendorTable'
 
 export default function AdminVendor() {
    const { vendors, status, deleteVendor } = useSelector(
@@ -59,159 +46,14 @@ export default function AdminVendor() {
    return (
       <>
          {isShowSpinner && <Spinner />}
-         <StyledTableCont component={Paper}>
-            <Table aria-label="simple table">
-               <TableHead>
-                  <TableRow>
-                     <StyledTableCellTitle align="inherit">
-                        №
-                     </StyledTableCellTitle>
-                     <StyledTableCellTitle align="inherit">
-                        Имя
-                     </StyledTableCellTitle>
-                     <StyledTableCellTitle align="inherit">
-                        Номер телефона
-                     </StyledTableCellTitle>
-                     <StyledTableCellTitle align="inherit">
-                        Почта
-                     </StyledTableCellTitle>
-                     <StyledTableCellTitle align="inherit">
-                        Количество книг
-                     </StyledTableCellTitle>
-                     <StyledTableCellTitle align="inherit">
-                        {' '}
-                     </StyledTableCellTitle>
-                  </TableRow>
-               </TableHead>
-               <TableBody>
-                  {vendors &&
-                     vendors.map((item, index) => (
-                        <StyledTableRow
-                           onClick={() => handleNavToVendorProfile(item.id)}
-                           sx={{
-                              '&.MuiTableRow-hover': {
-                                 '&:hover': {
-                                    backgroundColor: '#FFEEE6',
-                                    cursor: 'pointer',
-                                 },
-                              },
-                           }}
-                           hover
-                           key={item.id}
-                        >
-                           <StyledTableCell component="th" scope="row">
-                              {index + 1}
-                           </StyledTableCell>
-                           <StyledTableCell align="inherit">
-                              {item.firstName} {item.lastName}
-                           </StyledTableCell>
-                           <StyledTableCell align="inherit">
-                              {item.phoneNumber}
-                           </StyledTableCell>
-                           <StyledTableCell align="inherit">
-                              {item.email}
-                           </StyledTableCell>
-                           <StyledTableCell align="inherit">
-                              {item.quantityOfBooks}
-                           </StyledTableCell>
-                           <StyledTableCell
-                              align="inherit"
-                              onClick={(e) => e.stopPropagation()}
-                           >
-                              <IconButton onClick={handleOpenDeleteModal}>
-                                 <DeleteIcon />
-                              </IconButton>
-                              <Modal
-                                 open={isOpenDeleteModal}
-                                 onClose={handleCloseDeleteModal}
-                                 variant="mini"
-                                 width="460px"
-                                 height="161px"
-                              >
-                                 <StyledInfoText>
-                                    Вы уверены, что хотите удалить
-                                 </StyledInfoText>
-                                 <StyledInfoTitle>
-                                    “{item.firstName} {item.lastName}” ?
-                                 </StyledInfoTitle>
-                                 <StyledModalBtnCont>
-                                    <Button
-                                       variant="default"
-                                       background="white"
-                                       color="#A3A3A3"
-                                       onClick={handleCloseDeleteModal}
-                                    >
-                                       Отменить
-                                    </Button>
-                                    <Button
-                                       variant="default"
-                                       onClick={() =>
-                                          hendleDeleteVendor(item.id)
-                                       }
-                                    >
-                                       Удалить
-                                    </Button>
-                                 </StyledModalBtnCont>
-                              </Modal>
-                           </StyledTableCell>
-                        </StyledTableRow>
-                     ))}
-               </TableBody>
-            </Table>
-         </StyledTableCont>
+         <VendorTable
+            vendors={vendors}
+            handleDelete={hendleDeleteVendor}
+            handleNav={handleNavToVendorProfile}
+            openDelModal={isOpenDeleteModal}
+            handleOpenDelModal={handleOpenDeleteModal}
+            handleCloseDelModal={handleCloseDeleteModal}
+         />
       </>
    )
 }
-
-const StyledTableCellTitle = styled(TableCell)`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 700;
-   font-size: 14px;
-   line-height: 19px;
-   color: #222222;
-`
-const StyledTableCell = styled(TableCell)`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 400;
-   font-size: 14px;
-   line-height: 130%;
-   color: #222222;
-   border-bottom: none;
-`
-const StyledTableRow = styled(TableRow)`
-   &.MuiTableRow-hover {
-      & :hover {
-         background: #ffeee6;
-         cursor: pointer;
-      }
-   }
-`
-const StyledTableCont = styled(TableContainer)`
-   border-radius: 0px;
-   box-shadow: 0px 0px;
-`
-
-const StyledInfoText = styled.span`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 400;
-   font-size: 14px;
-   line-height: 130%;
-   color: #222222;
-`
-const StyledModalBtnCont = styled.div`
-   display: flex;
-   justify-content: space-between;
-   width: 278px;
-   height: 42px;
-`
-const StyledInfoTitle = styled.p`
-   font-family: 'Open Sans';
-   font-style: normal;
-   font-weight: 600;
-   font-size: 14px;
-   line-height: 130%;
-   color: #222222;
-`
